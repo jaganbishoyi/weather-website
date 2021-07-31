38df3a0d4a7af67a1bdc4868648d88f0076280fd
Jagan Mohan Bishoyi <jagan.bishoyi@people10.com>
2021-07-31 11:47:17 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    if (!location) {
+        return messageOne.textContent = 'You must provide a location';
+    }
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
+});
